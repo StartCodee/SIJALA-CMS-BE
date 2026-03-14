a0e2fda780a562a95fbbf25a578772e97108559d
Leonard <leonarddamanik7@gmail.com>
2026-03-15 00:18:54 +0700

    add env and gitignore

--- db.js
+++ db.js
@@ -1,11 +1,18 @@
 const { Pool } = require("pg");
+require('dotenv').config();
+
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbName = process.env.DB_NAME;
+const dbPort = process.env.DB_PORT;
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "sijala_cms",
-  password: "P@ssw0rd",
-  port: 5432
+  user: dbUser,
+  host: dbHost,
+  database: dbName,
+  password: dbPassword,
+  port: dbPort
 });
 
 module.exports = pool;
